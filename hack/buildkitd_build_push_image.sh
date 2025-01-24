a884627fe0a4e49b5e7b1ef60b9799b3ff48bb6b
daturece <daturece@redhat.com>
2025-01-24 13:02:06 +0100

    change image

--- hack/buildkitd_build_push_image.sh
+++ hack/buildkitd_build_push_image.sh
@@ -14,6 +14,6 @@ buildctl --addr unix:///run/user/$UID/buildkit/buildkitd.sock build \
   --secret id=docker,src="$HIVE_ROOT/.tmp/_output/config.json" \
   --opt build-arg:EL8_BUILD_IMAGE=registry.ci.openshift.org/openshift/release:golang-1.22 \
   --opt build-arg:EL9_BUILD_IMAGE=registry.ci.openshift.org/openshift/release:golang-1.22 \
-  --opt build-arg:BASE_IMAGE=registry.ci.openshift.org/origin/4.16:base \
+  --opt build-arg:BASE_IMAGE=quay.io/centos/centos:stream9 \
   --opt build-arg:GO="CGO_ENABLED=0 go" \
   --output type=image,name=localhost:5000/hive:latest,push=true
